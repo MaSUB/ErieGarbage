2711df2807eca89f1a45b8bf6bd74e5a9a1434ca
Mason Toy <mason.toy@gmail.com>
2016-12-03 20:00:49 -0500

    Thangs
    help me.

---
+++ garbageDetails.php
@@ -0,0 +1,11 @@
+<?php //    this is the content for the UserHome page.
+include 'header.php';
+echo $userHeaderLoggedIn;
+?>
+        <div class="content-detail">
+            <div class="content-row">
+                
+            </div>
+        </div>
+    </body>
+</html>
---
+++ viewDuePayments.php
@@ -0,0 +1,9 @@
+<?php //    this is the content for the UserHome page.
+include 'header.php';
+echo $userHeaderLoggedIn;
+?>
+    
+
+
+  </body>
+</html>
